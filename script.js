e259478dce434c67d3b8e88cd58e0af71cc885e8
akankshaa-2512 <akankshaanand2512@gmail.com>
2024-08-06 20:49:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+dayNight.addEventListener("click",()=>{
+    banner.classList.toggle("night");
+})
+
+let typingEffect = new Typed("#text",{
+    strings:["Akanksha Anand","a student","a Web Developer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1500
+})
